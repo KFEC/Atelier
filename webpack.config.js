e0df4006c72e18c58c84f94d1503fb39d42b9988
akbarimo <mo.akbari15@gmail.com>
2022-11-30 18:25:57 -0800

    Finished setting up webpack and basic backend

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: path.join(__dirname, '/src', 'index.jsx'),
+  entry: path.join(__dirname, '/client/src', 'index.jsx'),
   output: {
     path: path.join(__dirname, 'client/dist'),
     filename: 'bundle.js',
